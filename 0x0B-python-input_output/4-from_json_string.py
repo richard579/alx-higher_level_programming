e990d8576158a4e5376ad8e4184000b37c0562c3
richard579 <richardbeng358@outlook.com>
2023-06-13 07:29:46 -0700

    101-stats.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns python data structure"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns an object(python data structure)
+    the json representation of an object"""
+    data = json.loads(my_str)
+    return data
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns dictionary description with sample data structure"""
+
+
+def class_to_json(obj):
+    """returns dictinary decription with sample data structure"""
+    return obj.__dict__
