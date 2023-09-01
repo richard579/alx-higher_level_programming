c9c36f603ad297cb24e1a5a34ff1ca7b783f37a6
richard579 <richardbeng358@outlook.com>
2023-09-01 05:12:39 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
