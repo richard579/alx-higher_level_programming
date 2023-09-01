d4ac4d5535a4d28dc1fb5761c5bbfa9cb9edbf9b
richard579 <richardbeng358@outlook.com>
2023-09-01 05:44:54 -0700

    script that takes in a URL and an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, 
-and displays the body of the response (decoded in utf-8)"""
+"""script that takes in a URL and an email"""
 import sys
 import urllib.parse
 import urllib.request
