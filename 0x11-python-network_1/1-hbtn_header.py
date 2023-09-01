390790ca391938b81193147338048708071a74c2
richard579 <richardbeng358@outlook.com>
2023-09-01 04:12:31 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-ID"))
+        print(dict(response.headers).get("X-Request-Id"))
