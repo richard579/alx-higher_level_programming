cd990cb581bc4c02b32d78e8aa4242365f49de51
richard579 <richardbeng358@outlook.com>
2023-09-26 04:14:30 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
   } else {
